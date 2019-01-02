f64de897f9ade3e5add0d7510be55ca790c0d84a
Aleksanteri Heliövaara <aleksanteriheliovaara@Aleksanteris-MacBook-Pro.local>
2019-01-02 12:18:15 +0200

    deploy push

--- src/components/card-grid/CardGrid.js
+++ src/components/card-grid/CardGrid.js
@@ -23,7 +23,7 @@ class CardGrid extends Component {
     return (
       <div>
             <Row>
-              { this.state.data.map((card, index) => 
+              { this.props.data && this.props.data.map((card, index) => 
                 <Col key={index} xs={12} sm={6} md={4} lg={4}><Card data={card} /></Col>
               )}
             </Row>
